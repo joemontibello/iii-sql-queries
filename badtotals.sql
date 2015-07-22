96bf3380ccadcb3abd5a43cbdca7777f68effb1a
joemontibello <josephmontibello@gmail.com>
2015-07-22 10:30:12 -0400

    badtotals - query to gather records where TOT CHKOUT < YTDCIRC + LYRCIRC feedbuilder - query to start work on producing rss feeds from Sierra

--- badtotals.sql
+++ badtotals.sql
@@ -1,5 +1,5 @@
-﻿select 
-record_metadata.record_num,
+﻿select distinct
+'i' || record_metadata.record_num || 'a' as record_num,
 bib_record_property.best_title,
 item_record.checkout_total,
 item_record.year_to_date_checkout_total,
@@ -9,8 +9,11 @@ from
 sierra_view.item_record join sierra_view.bib_record_item_record_link on item_record.id = bib_record_item_record_link.item_record_id,
 sierra_view.bib_record_property,
 sierra_view.record_metadata
+--sierra_view.varfield
 where
+--varfield.record_id = item_record.id
+--and (select not exists(select 1 from sierra_view.varfield where varfield.varfield_type_code = 'r'))
 item_record.checkout_total < (item_record.year_to_date_checkout_total + item_record.last_year_to_date_checkout_total)
 and bib_record_property.bib_record_id = bib_record_item_record_link.bib_record_id
-and record_metadata.record_type_code = 'i'
-and record_metadata.id = item_record.id
+and (record_metadata.record_type_code = 'i'and record_metadata.id = item_record.id)
+--and item_record.save_checkout_total is not distinct from null
---
+++ feedbuilder.sql
@@ -0,0 +1,17 @@
+﻿SELECT 
+  bib_record.id
+
+FROM
+  sierra_view.bib_record,
+  sierra_view.item_record,
+  sierra_view.order_record,
+  sierra_view.bib_record_item_record_link,
+  sierra_view.bib_record_order_record_link  
+
+WHERE
+  bib_record.id = bib_record_item_record_link.bib_record_id
+  AND bib_record.id = bib_record_order_record_link.bib_record_id
+  AND item_record.id = bib_record_item_record_link.item_record_id
+  AND order_record.id = bib_record_order_record_link.order_record_id
+  AND order_record_cmf.order_record_id = order_record.id
+  AND bib_record.bcode3 = '-' -- bcode3 = BIB STATUS
