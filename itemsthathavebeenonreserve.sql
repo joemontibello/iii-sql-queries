d41d505148712dd5942e6b79bfea8015abbe3f4d
joemontibello <josephmontibello@gmail.com>
2017-02-07 10:55:20 -0500

    new query

---
+++ itemsthathavebeenonreserve.sql
@@ -0,0 +1,8 @@
+﻿SELECT id2reckey(item_record.id)
+
+FROM
+sierra_view.item_record JOIN sierra_view.varfield on item_record.id = varfield.record_id
+
+WHERE
+varfield_type_code = 'r'
+AND field_content LIKE '%RESERVE%'
