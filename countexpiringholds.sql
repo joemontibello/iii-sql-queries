98ff178241d1e4f0e3b228cd06394648b120f36f
joemontibello <josephmontibello@gmail.com>
2017-02-24 09:38:35 -0500

    new query to count holds expiring today

---
+++ countexpiringholds.sql
@@ -0,0 +1,8 @@
+﻿--Counts the number of holds that expire today.
+--Presumably, once the holdshelf is cleared and items that
+--expire are checked in, this number should be zero.
+
+SELECT COUNT(*) from 
+sierra_view.hold
+WHERE
+to_char(now(), 'YYYY-MM-DD') = to_char(expires_gmt, 'YYYY-MM-DD')
