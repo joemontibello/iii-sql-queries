a32c3058fdc37da423d21af4afc66b771fd9ee23
joemontibello <josephmontibello@gmail.com>
2017-03-08 10:58:59 -0500

    generalizing patwithodues.sql

--- patwithodues.sql
+++ patwithodues.sql
@@ -1,7 +1,11 @@
-﻿--get all patrons with overdue fines.
-SELECT id2reckey(r.id) as pnum,
+﻿--all patron fines.
+--see http://techdocs.iii.com/sierradna/Home.html?viewGroupName=Circulation#fine
+--for list of what each charge_code means
+
+SELECT r.id,
 fn.last_name,
 fn.first_name,
+f.charge_code,
 f.item_charge_amt,
 f.processing_fee_amt,
 f.billing_fee_amt,
@@ -10,5 +14,5 @@ f.item_charge_amt + f.processing_fee_amt + f.billing_fee_amt as total
 FROM 
 sierra_view.fine f JOIN sierra_view.patron_record r on f.patron_record_id = r.id
 JOIN sierra_view.patron_record_fullname fn on fn.patron_record_id = r.id
-WHERE 
-f.charge_code = '2'
+ORDER BY id
+
