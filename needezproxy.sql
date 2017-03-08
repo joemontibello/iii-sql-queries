c2079389b59ae4a6c729dbd61f042ab5a8f015fb
joemontibello <josephmontibello@gmail.com>
2017-03-08 10:43:09 -0500

    needezproxy and patwithodues added

---
+++ needezproxy.sql
@@ -0,0 +1,19 @@
+﻿--Query pulls a list of records that should have 
+--EZProxy but don't have it.
+
+SELECT id2reckey(b.id),
+s.content
+
+FROM
+sierra_view.subfield_view s INNER JOIN sierra_view.bib_record b ON b.id = s.record_id
+WHERE
+b.bcode1 = 'w'
+AND s.record_type_code = 'b'
+AND s.marc_tag = '856'
+AND b.bcode3 = '-'
+AND s.tag = 'u'
+AND s.content NOT LIKE '%idm%'
+and s.content NOT LIKE '%ddlp%'
+and s.content NOT LIKE 'http://ieeexplore.ieee.org/servlet/opac?punumber=%'
+and s.content NOT LIKE 'http://digital.casalini.it/%'
+ORDER BY s.content
---
+++ patwithodues.sql
@@ -0,0 +1,14 @@
+﻿--get all patrons with overdue fines.
+SELECT id2reckey(r.id) as pnum,
+fn.last_name,
+fn.first_name,
+f.item_charge_amt,
+f.processing_fee_amt,
+f.billing_fee_amt,
+f.item_charge_amt + f.processing_fee_amt + f.billing_fee_amt as total
+
+FROM 
+sierra_view.fine f JOIN sierra_view.patron_record r on f.patron_record_id = r.id
+JOIN sierra_view.patron_record_fullname fn on fn.patron_record_id = r.id
+WHERE 
+f.charge_code = '2'
