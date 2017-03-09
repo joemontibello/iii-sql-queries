efd7a1a77428ce392891d570f85cbbd5c86ce0ec
joemontibello <josephmontibello@gmail.com>
2017-03-09 10:21:51 -0500

    Find records with bad subfield tags

---
+++ badsubfieldtags.sql
@@ -0,0 +1,13 @@
+﻿-- Find subfield tags that are not 
+-- a-z or 0-9, and return record numbers.
+
+SELECT id2reckey(record_id) recnum,
+marc_tag,
+tag,
+content
+
+FROM 
+sierra_view.subfield s 
+WHERE
+s.tag !~ '[a-z 0-9]'
+ORDER BY recnum
